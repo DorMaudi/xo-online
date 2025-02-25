6f0646e96e4873d0d5dee2807430a2f3fc5f301b
Dor Maudi <dor.maudi9@gmail.com>
2025-02-25 16:31:06 +0200

    added ip, port

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
+import socket
+import struct
 
+IP = "127.0.0.1"
+PORT = 8989
 
 if __name__ == '__main__':
     print('Hello world')
--- server.py
+++ server.py
@@ -1,4 +1,14 @@
+import socket
+import struct
 
+IP = "127.0.0.1"
+PORT = 8989
+
+def e_send():
+    pass
+
+def e_recv():
+    pass
 
 if __name__ == '__main__':
     pass
