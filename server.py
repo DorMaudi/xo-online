11aaf8287001f1d3ff604980119d1feac5bda7bf
Sapir Natanov <157406753+SapirNatanov@users.noreply.github.com>
2025-02-25 17:34:33 +0200

    update server

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import socket
 import struct
 
-IP = "127.0.0.1"
 PORT = 8989
 
 def e_send():
@@ -11,6 +10,36 @@ def e_recv():
     pass
 
 if __name__ == '__main__':
-    pass
+    # next create a socket object 
+    s = socket.socket()		 
+    print ("Socket successfully created")
+
+    # we have not typed any ip in the ip field 
+    # instead we have inputted an empty string 
+    # this makes the server listen to requests 
+    # coming from other computers on the network 
+    s.bind(('', PORT))		 
+    print ("socket binded to %s" %(PORT)) 
+
+    # put the socket into listening mode 
+    s.listen()	 
+    print ("socket is listening")		 
+
+    # a forever loop until we interrupt it or 
+    # an error occurs 
+    while True: 
+        # Establish connection with client. 
+        c, addr = s.accept()	 
+        print ('Got connection from', addr )
+
+        # send a thank you message to the client. encoding to send byte type. 
+        c.send('Thank you for connecting'.encode()) 
+
+        # Close the connection with the client 
+        c.close()
+
+        # Breaking once connection closed
+        break
+
 
 
